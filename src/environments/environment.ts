34e4a2505a198b1cb51c62fde650583eef7966dd
Juan Ruíz <juan.ruiz.22.03.00@gmail.com>
2020-07-02 21:28:17 -0500

    Conexión a Firebase
    Se hicieron las configuraciones iniciales del proyecto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCfINaY7n_hBXMn1_0vVwFeJoOxgZ_Z04U',
+    authDomain: 'firechat-2ef05.firebaseapp.com',
+    databaseURL: 'https://firechat-2ef05.firebaseio.com',
+    projectId: 'firechat-2ef05',
+    storageBucket: 'firechat-2ef05.appspot.com',
+    messagingSenderId: '1010311995205',
+    appId: '1:1010311995205:web:e2b083d9220e8c8419282c'
+  }
 };
 
 /*
